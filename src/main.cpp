9c7b17800bd69d38304ffd2ebfe2cb41c5e6e21d
Multipixels <motorger@mcmaster.ca>
2025-05-30 21:36:45 -0400

    used wrong glad generator, create test

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,51 @@
 #include <iostream>
 
+#include "glad.h"
 #include "SDL3/SDL.h"
-#include "gl.h"
 
 int main() {
+	SDL_Init(SDL_INIT_VIDEO);
 
-	std::cout << "Hello, world!a";
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_Window* window = SDL_CreateWindow("OpenGL Window",
+		800, 600,
+		SDL_WINDOW_OPENGL);
+
+	SDL_GLContext glContext = SDL_GL_CreateContext(window);
+	if (!glContext) {
+		// Handle error
+		SDL_Log("OpenGL context creation failed: %s", SDL_GetError());
+	}
+
+	if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+		SDL_Log("Failed to initialize GLAD");
+	}
+
+	SDL_GL_SetSwapInterval(1);
+
+	bool running = true;
+	SDL_Event event;
+
+	while (running) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_QUIT) {
+				running = false;
+			}
+		}
+
+		// Rendering commands
+		glClearColor(0.1f, 0.1f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		SDL_GL_SwapWindow(window);
+	}
+
+	SDL_GL_DestroyContext(glContext);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
 	return 0;
 }
