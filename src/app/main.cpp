c3e2e1ef0f59b7e7673501fbdca262327d19293b
Multipixels <motorger@mcmaster.ca>
2025-08-05 15:28:30 -0400

    make a window, closes on ESC

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,14 +2,73 @@
 
 #include "rose.h"
 
-int main(int argc, char* argv[]) 
-{
-	if (argc != 2)
-	{
-		//std::cout << "Expected file path.";
-		//return 1;
-	}
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-	rose::Rose rose;
-	return rose.runGame();
+/* We will use this renderer to draw into this window every frame. */
+static SDL_Window* window = NULL;
+static SDL_Renderer* renderer = NULL;
+
+static SDL_AppResult handle_key_event(SDL_Scancode key_code)
+{
+    switch (key_code) {
+    case SDL_SCANCODE_ESCAPE:
+        return SDL_APP_SUCCESS;
+    default:
+        break;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
+{
+    SDL_SetAppMetadata("RoseGB", "1.0", "com.multipixels.rose-gb");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("RoseGB", 640, 576, 0, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+    switch (event->type) {
+    case SDL_EVENT_QUIT:
+        return SDL_APP_SUCCESS;
+    case SDL_EVENT_KEY_DOWN:
+        return handle_key_event(event->key.scancode);
+    default:
+        break;
+    }
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+    SDL_SetRenderDrawColorFloat(renderer, 1, 1, 1, SDL_ALPHA_OPAQUE_FLOAT);  /* new color, full alpha. */
+
+    /* clear the window to the draw color. */
+    SDL_RenderClear(renderer);
+
+    /* put the newly-cleared rendering on the screen. */
+    SDL_RenderPresent(renderer);
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+    /* SDL will clean up the window/renderer for us. */
 }
