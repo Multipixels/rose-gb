5798cea9100a15c355c11a3dbbbd261cb483fe58
Multipixels <motorger@mcmaster.ca>
2025-12-03 21:29:24 -0500

    testing: automate the mooneye tests better, update status of tests

--- tests/mooneye/mooneye-test.cpp
+++ tests/mooneye/mooneye-test.cpp
@@ -12,36 +12,36 @@ TEST(Mooneye, AcceptanceMiscell)
 
 	std::vector<TestCase> testCases{
 		// { "../resources/mooneye/acceptance/add_sp_e_timing.gb",			1000000 }, // 0
-		// { "../resources/mooneye/acceptance/boot_div-dmgABCmgb.gb",		500000 }, // 396
-		// { "../resources/mooneye/acceptance/boot_hwio-dmgABCmgb.gb",		500000 }, // 396
-		// { "../resources/mooneye/acceptance/boot_regs-dmgABC.gb",			500000 }, // 396
+		// { "../resources/mooneye/acceptance/boot_div-dmgABCmgb.gb",		 500000 }, // 396
+		// { "../resources/mooneye/acceptance/boot_hwio-dmgABCmgb.gb",		 500000 }, // 396
+		// { "../resources/mooneye/acceptance/boot_regs-dmgABC.gb",			 500000 }, // 396
 		// { "../resources/mooneye/acceptance/call_cc_timing.gb",			1000000 }, // 0
 		// { "../resources/mooneye/acceptance/call_cc_timing2.gb",			1000000 }, // 0
 		// { "../resources/mooneye/acceptance/call_timing.gb",				1000000 }, // 0
 		// { "../resources/mooneye/acceptance/call_timing2.gb",				1000000 }, // 0
-		// { "../resources/mooneye/acceptance/div_timing.gb",				1000000 }, // Passed
-		// { "../resources/mooneye/acceptance/di_timing-GS.gb",				500000 }, // 110
-		// { "../resources/mooneye/acceptance/ei_sequence.gb",				1000000 }, // Passed
-		// { "../resources/mooneye/acceptance/ei_timing.gb",				1000000 }, // Passed
+		{ "../resources/mooneye/acceptance/div_timing.gb",						 -1 }, // Passed
+		// { "../resources/mooneye/acceptance/di_timing-GS.gb",				 500000 }, // 110
+		{ "../resources/mooneye/acceptance/ei_sequence.gb",						 -1 }, // Passed
+		{ "../resources/mooneye/acceptance/ei_timing.gb",						 -1 }, // Passed
 		// { "../resources/mooneye/acceptance/halt_ime0_ei.gb",				1000000 }, // 0
 		// { "../resources/mooneye/acceptance/halt_ime0_nointr_timing.gb",	1000000 }, // 0
-		// { "../resources/mooneye/acceptance/halt_ime1_timing.gb",			1000000 }, // Passed
+		{ "../resources/mooneye/acceptance/halt_ime1_timing.gb",				 -1 }, // Passed
 		// { "../resources/mooneye/acceptance/halt_ime1_timing2-GS.gb",		1000000 }, // 0
-		// { "../resources/mooneye/acceptance/if_ie_registers.gb",			1000000 }, // Passed
-		// { "../resources/mooneye/acceptance/intr_timing.gb",				500000 }, // 396
+		{ "../resources/mooneye/acceptance/if_ie_registers.gb",					 -1 }, // Passed
+		// { "../resources/mooneye/acceptance/intr_timing.gb",				 500000 }, // 396
 		// { "../resources/mooneye/acceptance/jp_cc_timing.gb",				1000000 }, // 0
 		// { "../resources/mooneye/acceptance/jp_timing.gb",				1000000 }, // 0
 		// { "../resources/mooneye/acceptance/ld_hl_sp_e_timing.gb",		1000000 }, // 0
 		// { "../resources/mooneye/acceptance/oam_dma_restart.gb",			1000000 }, // 0
-		// { "../resources/mooneye/acceptance/oam_dma_start.gb",			500000 }, // 323
+		// { "../resources/mooneye/acceptance/oam_dma_start.gb",			 500000 }, // 323
 		// { "../resources/mooneye/acceptance/oam_dma_timing.gb",			1000000 }, // 0
-		// { "../resources/mooneye/acceptance/pop_timing.gb",				1000000 }, // Passed
+		{ "../resources/mooneye/acceptance/pop_timing.gb",						 -1 }, // Passed
 		// { "../resources/mooneye/acceptance/push_timing.gb",				1000000 }, // 0
-		// { "../resources/mooneye/acceptance/rapid_di_ei.gb",				1000000 }, // Passed
-		// { "../resources/mooneye/acceptance/reti_intr_timing.gb",			500000 }, // 396
-		// { "../resources/mooneye/acceptance/reti_timing.gb",				500000 }, // 420
-		// { "../resources/mooneye/acceptance/ret_cc_timing.gb",			500000 }, // 418
-		// { "../resources/mooneye/acceptance/ret_timing.gb",				500000 }, // 420
+		{ "../resources/mooneye/acceptance/rapid_di_ei.gb",						 -1 }, // Passed
+		// { "../resources/mooneye/acceptance/reti_intr_timing.gb",			 500000 }, // 396
+		// { "../resources/mooneye/acceptance/reti_timing.gb",				 500000 }, // 420
+		// { "../resources/mooneye/acceptance/ret_cc_timing.gb",			 500000 }, // 418
+		// { "../resources/mooneye/acceptance/ret_timing.gb",				 500000 }, // 420
 		// { "../resources/mooneye/acceptance/rst_timing.gb",				1000000 }, // 0
 	};
 
@@ -52,22 +52,25 @@ TEST(Mooneye, AcceptanceMiscell)
 
 		int instructionsRan = 0;
 		rose_core::u8 console = rose.tempReadConsole();
-		while (instructionsRan < test.maxInstructions)
+
+		int outputCounter = 0;
+		while (instructionsRan < 1000000)
 		{
 			if (rose.tempReadConsole() != console)
 			{
 				console = rose.tempReadConsole();
-				std::cout << (int)console << " ";
+				EXPECT_EQ(outputExpected[outputCounter], console)
+					<< "Failed: " << test.romFile;
+				outputCounter++;
 			}
+
+			if (outputCounter == 6) break;
 
 			ASSERT_NO_FATAL_FAILURE(rose.stepForward());
 			instructionsRan++;
 		}
-		
-		auto reg = rose.viewCPU().viewRegisters();
-		EXPECT_EQ(reg.b + reg.c + reg.d + reg.e + reg.h + reg.l, 84)
-			<< "Failed: " << test.romFile;
-		std::cout << std::endl;
+
+		if (instructionsRan >= 1000000) ADD_FAILURE() << "Failed (timeout): " << test.romFile;
 	}
 }
 
@@ -80,9 +83,9 @@ TEST(Mooneye, AcceptanceBits)
 	} TestCase;
 
 	std::vector<TestCase> testCases{
-		// { "../resources/mooneye/acceptance/bits/mem_oam.gb",			1000000 }, // untested
-		// { "../resources/mooneye/acceptance/bits/reg_f.gb",			1000000 }, // untested
-		// { "../resources/mooneye/acceptance/bits/unused_hwio-GS.gb",	1000000 }, // untested
+		{ "../resources/mooneye/acceptance/bits/mem_oam.gb",				 -1 }, // passed
+		{ "../resources/mooneye/acceptance/bits/reg_f.gb",					 -1 }, // passed
+		// { "../resources/mooneye/acceptance/bits/unused_hwio-GS.gb",	1000000 }, // failed
 	};
 
 	for (TestCase test : testCases)
@@ -92,22 +95,25 @@ TEST(Mooneye, AcceptanceBits)
 
 		int instructionsRan = 0;
 		rose_core::u8 console = rose.tempReadConsole();
-		while (instructionsRan < test.maxInstructions)
+
+		int outputCounter = 0;
+		while (instructionsRan < 1000000)
 		{
 			if (rose.tempReadConsole() != console)
 			{
 				console = rose.tempReadConsole();
-				std::cout << (int)console << " ";
+				EXPECT_EQ(outputExpected[outputCounter], console)
+					<< "Failed: " << test.romFile;
+				outputCounter++;
 			}
+
+			if (outputCounter == 6) break;
 
 			ASSERT_NO_FATAL_FAILURE(rose.stepForward());
 			instructionsRan++;
 		}
 
-		auto reg = rose.viewCPU().viewRegisters();
-		EXPECT_EQ(reg.b + reg.c + reg.d + reg.e + reg.h + reg.l, 84)
-			<< "Failed: " << test.romFile;
-		std::cout << std::endl;
+		if (instructionsRan >= 1000000) ADD_FAILURE() << "Failed (timeout): " << test.romFile;
 	}
 }
 
@@ -121,7 +127,7 @@ TEST(Mooneye, AcceptanceInstr)
 	} TestCase;
 
 	std::vector<TestCase> testCases{
-		// { "../resources/mooneye/acceptance/instr/daa.gb, 1000000 }, // untested
+		{ "../resources/mooneye/acceptance/instr/daa.gb", -1 }, // passed
 	};
 
 	for (TestCase test : testCases)
@@ -131,22 +137,25 @@ TEST(Mooneye, AcceptanceInstr)
 
 		int instructionsRan = 0;
 		rose_core::u8 console = rose.tempReadConsole();
-		while (instructionsRan < test.maxInstructions)
+
+		int outputCounter = 0;
+		while (instructionsRan < 1000000)
 		{
 			if (rose.tempReadConsole() != console)
 			{
 				console = rose.tempReadConsole();
-				std::cout << (int)console << " ";
+				EXPECT_EQ(outputExpected[outputCounter], console)
+					<< "Failed: " << test.romFile;
+				outputCounter++;
 			}
+
+			if (outputCounter == 6) break;
 
 			ASSERT_NO_FATAL_FAILURE(rose.stepForward());
 			instructionsRan++;
 		}
 
-		auto reg = rose.viewCPU().viewRegisters();
-		EXPECT_EQ(reg.b + reg.c + reg.d + reg.e + reg.h + reg.l, 84)
-			<< "Failed: " << test.romFile;
-		std::cout << std::endl;
+		if (instructionsRan >= 1000000) ADD_FAILURE() << "Failed (timeout): " << test.romFile;
 	}
 }
 
@@ -159,7 +168,7 @@ TEST(Mooneye, AcceptanceInterrupts)
 	} TestCase;
 
 	std::vector<TestCase> testCases{
-		// { "../resources/mooneye/acceptance/interrupts/ie_push.gb, 1000000 }, // untested
+		// { "../resources/mooneye/acceptance/interrupts/ie_push.gb", 1000000 }, // failed
 	};
 
 	for (TestCase test : testCases)
@@ -169,22 +178,25 @@ TEST(Mooneye, AcceptanceInterrupts)
 
 		int instructionsRan = 0;
 		rose_core::u8 console = rose.tempReadConsole();
-		while (instructionsRan < test.maxInstructions)
+
+		int outputCounter = 0;
+		while (instructionsRan < 1000000)
 		{
 			if (rose.tempReadConsole() != console)
 			{
 				console = rose.tempReadConsole();
-				std::cout << (int)console << " ";
+				EXPECT_EQ(outputExpected[outputCounter], console)
+					<< "Failed: " << test.romFile;
+				outputCounter++;
 			}
+
+			if (outputCounter == 6) break;
 
 			ASSERT_NO_FATAL_FAILURE(rose.stepForward());
 			instructionsRan++;
 		}
 
-		auto reg = rose.viewCPU().viewRegisters();
-		EXPECT_EQ(reg.b + reg.c + reg.d + reg.e + reg.h + reg.l, 84)
-			<< "Failed: " << test.romFile;
-		std::cout << std::endl;
+		if (instructionsRan >= 1000000) ADD_FAILURE() << "Failed (timeout): " << test.romFile;
 	}
 }
 
@@ -199,9 +211,9 @@ TEST(Mooneye, AcceptanceOamDma)
 	} TestCase;
 
 	std::vector<TestCase> testCases{
-		// { "../resources/mooneye/acceptance/oam_dma/basic.gb,			1000000 }, // untested
-		// { "../resources/mooneye/acceptance/oam_dma/reg_read.gb,		1000000 }, // untested
-		// { "../resources/mooneye/acceptance/oam_dma/sources-GS.gb,	1000000 }, // untested
+		// { "../resources/mooneye/acceptance/oam_dma/basic.gb",		1000000 }, // failed
+		{ "../resources/mooneye/acceptance/oam_dma/reg_read.gb",			 -1 }, // passed
+		// { "../resources/mooneye/acceptance/oam_dma/sources-GS.gb",	1000000 }, // failed
 	};
 
 	for (TestCase test : testCases)
@@ -211,22 +223,25 @@ TEST(Mooneye, AcceptanceOamDma)
 
 		int instructionsRan = 0;
 		rose_core::u8 console = rose.tempReadConsole();
-		while (instructionsRan < test.maxInstructions)
+
+		int outputCounter = 0;
+		while (instructionsRan < 1000000)
 		{
 			if (rose.tempReadConsole() != console)
 			{
 				console = rose.tempReadConsole();
-				std::cout << (int)console << " ";
+				EXPECT_EQ(outputExpected[outputCounter], console)
+					<< "Failed: " << test.romFile;
+				outputCounter++;
 			}
+
+			if (outputCounter == 6) break;
 
 			ASSERT_NO_FATAL_FAILURE(rose.stepForward());
 			instructionsRan++;
 		}
 
-		auto reg = rose.viewCPU().viewRegisters();
-		EXPECT_EQ(reg.b + reg.c + reg.d + reg.e + reg.h + reg.l, 84)
-			<< "Failed: " << test.romFile;
-		std::cout << std::endl;
+		if (instructionsRan >= 1000000) ADD_FAILURE() << "Failed (timeout): " << test.romFile;
 	}
 }
 
@@ -241,18 +256,18 @@ TEST(Mooneye, AcceptancePPU)
 	} TestCase;
 
 	std::vector<TestCase> testCases{
-		// { "../resources/mooneye/acceptance/ppu/hblank_ly_scx_timing-GS.gb,		1000000 }, // untested
-		// { "../resources/mooneye/acceptance/ppu/intr_1_2_timing-GS.gb,			1000000 }, // untested
-		// { "../resources/mooneye/acceptance/ppu/intr_2_0_timing.gb,				1000000 }, // untested
-		// { "../resources/mooneye/acceptance/ppu/intr_2_mode0_timing.gb,			1000000 }, // untested
-		// { "../resources/mooneye/acceptance/ppu/intr_2_mode0_timing_sprites.gb,	1000000 }, // untested
-		// { "../resources/mooneye/acceptance/ppu/intr_2_mode3_timing.gb,			1000000 }, // untested
-		// { "../resources/mooneye/acceptance/ppu/intr_2_oam_ok_timing.gb,			1000000 }, // untested
-		// { "../resources/mooneye/acceptance/ppu/lcdon_timing-GS.gb,				1000000 }, // untested
-		// { "../resources/mooneye/acceptance/ppu/lcdon_write_timing-GS.gb,			1000000 }, // untested
-		// { "../resources/mooneye/acceptance/ppu/stat_irq_blocking.gb,				1000000 }, // untested
-		// { "../resources/mooneye/acceptance/ppu/stat_lyc_onoff.gb,				1000000 }, // untested
-		// { "../resources/mooneye/acceptance/ppu/vblank_stat_intr-GS.gb,			1000000 }, // untested
+		// { "../resources/mooneye/acceptance/ppu/hblank_ly_scx_timing-GS.gb",		1000000 }, // failed
+		// { "../resources/mooneye/acceptance/ppu/intr_1_2_timing-GS.gb",			1000000 }, // failed
+		// { "../resources/mooneye/acceptance/ppu/intr_2_0_timing.gb",				1000000 }, // failed
+		// { "../resources/mooneye/acceptance/ppu/intr_2_mode0_timing.gb",			1000000 }, // failed
+		// { "../resources/mooneye/acceptance/ppu/intr_2_mode0_timing_sprites.gb",	1000000 }, // failed
+		// { "../resources/mooneye/acceptance/ppu/intr_2_mode3_timing.gb",			1000000 }, // failed
+		// { "../resources/mooneye/acceptance/ppu/intr_2_oam_ok_timing.gb",			1000000 }, // failed
+		// { "../resources/mooneye/acceptance/ppu/lcdon_timing-GS.gb",				1000000 }, // failed
+		// { "../resources/mooneye/acceptance/ppu/lcdon_write_timing-GS.gb",		1000000 }, // failed
+		// { "../resources/mooneye/acceptance/ppu/stat_irq_blocking.gb",			1000000 }, // failed
+		// { "../resources/mooneye/acceptance/ppu/stat_lyc_onoff.gb",				1000000 }, // failed
+		// { "../resources/mooneye/acceptance/ppu/vblank_stat_intr-GS.gb",			1000000 }, // infinite loop???
 	};
 
 	for (TestCase test : testCases)
@@ -262,22 +277,25 @@ TEST(Mooneye, AcceptancePPU)
 
 		int instructionsRan = 0;
 		rose_core::u8 console = rose.tempReadConsole();
-		while (instructionsRan < test.maxInstructions)
+
+		int outputCounter = 0;
+		while (instructionsRan < 1000000)
 		{
 			if (rose.tempReadConsole() != console)
 			{
 				console = rose.tempReadConsole();
-				std::cout << (int)console << " ";
+				EXPECT_EQ(outputExpected[outputCounter], console)
+					<< "Failed: " << test.romFile;
+				outputCounter++;
 			}
+
+			if (outputCounter == 6) break;
 
 			ASSERT_NO_FATAL_FAILURE(rose.stepForward());
 			instructionsRan++;
 		}
 
-		auto reg = rose.viewCPU().viewRegisters();
-		EXPECT_EQ(reg.b + reg.c + reg.d + reg.e + reg.h + reg.l, 84)
-			<< "Failed: " << test.romFile;
-		std::cout << std::endl;
+		if (instructionsRan >= 1000000) ADD_FAILURE() << "Failed (timeout): " << test.romFile;
 	}
 }
 
@@ -291,7 +309,7 @@ TEST(Mooneye, AcceptanceSerial)
 	} TestCase;
 
 	std::vector<TestCase> testCases{
-		// { "../resources/mooneye/acceptance/serial/boot_sclk_align-dmgABCmgb.gb,	1000000 }, // untested
+		// { "../resources/mooneye/acceptance/serial/boot_sclk_align-dmgABCmgb.gb",	1000000 }, // failed
 	};
 
 	for (TestCase test : testCases)
@@ -301,22 +319,25 @@ TEST(Mooneye, AcceptanceSerial)
 
 		int instructionsRan = 0;
 		rose_core::u8 console = rose.tempReadConsole();
-		while (instructionsRan < test.maxInstructions)
+
+		int outputCounter = 0;
+		while (instructionsRan < 1000000)
 		{
 			if (rose.tempReadConsole() != console)
 			{
 				console = rose.tempReadConsole();
-				std::cout << (int)console << " ";
+				EXPECT_EQ(outputExpected[outputCounter], console)
+					<< "Failed: " << test.romFile;
+				outputCounter++;
 			}
+
+			if (outputCounter == 6) break;
 
 			ASSERT_NO_FATAL_FAILURE(rose.stepForward());
 			instructionsRan++;
 		}
 
-		auto reg = rose.viewCPU().viewRegisters();
-		EXPECT_EQ(reg.b + reg.c + reg.d + reg.e + reg.h + reg.l, 84)
-			<< "Failed: " << test.romFile;
-		std::cout << std::endl;
+		if (instructionsRan >= 1000000) ADD_FAILURE() << "Failed (timeout): " << test.romFile;
 	}
 }
 
@@ -330,19 +351,19 @@ TEST(Mooneye, AcceptanceTimer)
 	} TestCase;
 
 	std::vector<TestCase> testCases{
-		// { "../resources/mooneye/acceptance/timer/div_write.gb,	1000000 }, // untested
-		// { "../resources/mooneye/acceptance/timer/rapid_toggle.gb,	1000000 }, // untested
-		// { "../resources/mooneye/acceptance/timer/tim00.gb,	1000000 }, // untested
-		// { "../resources/mooneye/acceptance/timer/tim00_div_trigger.gb,	1000000 }, // untested
-		// { "../resources/mooneye/acceptance/timer/tim01.gb,	1000000 }, // untested
-		// { "../resources/mooneye/acceptance/timer/tim01_div_trigger.gb,	1000000 }, // untested
-		// { "../resources/mooneye/acceptance/timer/tim10..gb,	1000000 }, // untested
-		// { "../resources/mooneye/acceptance/timer/tim10_div_trigger.gb,	1000000 }, // untested
-		// { "../resources/mooneye/acceptance/timer/tim11.gb,	1000000 }, // untested
-		// { "../resources/mooneye/acceptance/timer/tim11_div_trigger.gb,	1000000 }, // untested
-		// { "../resources/mooneye/acceptance/timer/tima_reload.gb,	1000000 }, // untested
-		// { "../resources/mooneye/acceptance/timer/tima_write_reloading.gb,	1000000 }, // untested
-		// { "../resources/mooneye/acceptance/timer/tma_write_reloading.gb,	1000000 }, // untested
+		{ "../resources/mooneye/acceptance/timer/div_write.gb",						 -1 }, // passed
+		// { "../resources/mooneye/acceptance/timer/rapid_toggle.gb",			1000000 }, // failed
+		{ "../resources/mooneye/acceptance/timer/tim00.gb",							 -1 }, // passed
+		// { "../resources/mooneye/acceptance/timer/tim00_div_trigger.gb",		1000000 }, // failed
+		{ "../resources/mooneye/acceptance/timer/tim01.gb",							 -1 }, // passed
+		// { "../resources/mooneye/acceptance/timer/tim01_div_trigger.gb",		1000000 }, // failed
+		// { "../resources/mooneye/acceptance/timer/tim10.gb",					1000000 }, // failed
+		// { "../resources/mooneye/acceptance/timer/tim10_div_trigger.gb",		1000000 }, // failed
+		{ "../resources/mooneye/acceptance/timer/tim11.gb",							 -1 }, // passed
+		// { "../resources/mooneye/acceptance/timer/tim11_div_trigger.gb",		1000000 }, // failed
+		// { "../resources/mooneye/acceptance/timer/tima_reload.gb",			1000000 }, // failed
+		// { "../resources/mooneye/acceptance/timer/tima_write_reloading.gb",	1000000 }, // failed
+		// { "../resources/mooneye/acceptance/timer/tma_write_reloading.gb",	1000000 }, // failed
 	};
 
 	for (TestCase test : testCases)
@@ -352,21 +373,24 @@ TEST(Mooneye, AcceptanceTimer)
 
 		int instructionsRan = 0;
 		rose_core::u8 console = rose.tempReadConsole();
-		while (instructionsRan < test.maxInstructions)
+
+		int outputCounter = 0;
+		while (instructionsRan < 1000000)
 		{
 			if (rose.tempReadConsole() != console)
 			{
 				console = rose.tempReadConsole();
-				std::cout << (int)console << " ";
+				EXPECT_EQ(outputExpected[outputCounter], console)
+					<< "Failed: " << test.romFile;
+				outputCounter++;
 			}
+
+			if (outputCounter == 6) break;
 
 			ASSERT_NO_FATAL_FAILURE(rose.stepForward());
 			instructionsRan++;
 		}
 
-		auto reg = rose.viewCPU().viewRegisters();
-		EXPECT_EQ(reg.b + reg.c + reg.d + reg.e + reg.h + reg.l, 84)
-			<< "Failed: " << test.romFile;
-		std::cout << std::endl;
+		if (instructionsRan >= 1000000) ADD_FAILURE() << "Failed (timeout): " << test.romFile;
 	}
 }
--- tests/mooneye/mooneye-test.h
+++ tests/mooneye/mooneye-test.h
@@ -1,1 +1,3 @@
 #include "rose.h"
+
+rose_core::u8 outputExpected[6] = { 3, 5, 8, 13, 21, 34 };
